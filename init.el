701804335f4aa186a4c7677e6e6f8d30f1bcb8d1
Takuya Tsuchida <tsucchi@cpan.org>
2012-07-24 07:03:07 +0900

    起動時にエラーになっていたので直した

--- init.el
+++ init.el
@@ -911,6 +911,28 @@
 (when (require 'yasnippet-config)
   (yas/setup "~/.emacs.d/plugins/yasnippet"))
 
+;;
+;; markdown-mode
+;; http://jblevins.org/projects/markdown-mode/markdown-mode.el
+(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
+(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
+;;先頭行にヘッダを入れる(日記専用)
+(defun md-insert-header ()
+  (interactive)
+  (goto-char 1)
+  (insert "---\n")
+  (insert "layout: post\n")
+  (insert "category: \n")
+  (insert "tags: \n")
+  (insert "title: \n")
+  (insert "---\n")
+  (insert "{% include JB/setup %}\n"))
+(add-hook 'markdown-mode-hook
+		  '(lambda ()
+			 (define-key markdown-mode-map "\C-ci" 'md-insert-header)))
+
+
+
 ;; ---------------------------- 以下は原則として変更しない ------------------------------------
 ;; emacs23.2 (以降?)では color-theme 使うとフレームサイズが勝手に変更されるのでここで実施
 (add-hook 'window-setup-hook
