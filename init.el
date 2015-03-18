9dbfacef9433680ac5821a5ba84db4a1b72f0e5d
Takuya Tsuchida <takuya.tsuchida@gmail.com>
2015-03-18 14:19:40 +0900

    updated

--- init.el
+++ init.el
@@ -969,8 +969,10 @@
 ;;
 ;; markdown-mode
 ;; http://jblevins.org/projects/markdown-mode/markdown-mode.el
-(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
-(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
+;;(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
+(autoload 'gfm-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
+(setq auto-mode-alist (cons '("\\.md" . gfm-mode) auto-mode-alist))
+
 ;;先頭行にヘッダを入れる(日記専用)
 (defun md-insert-header ()
   (interactive)
@@ -984,6 +986,7 @@
   (insert "{% include JB/setup %}\n"))
 (add-hook 'markdown-mode-hook
 		  '(lambda ()
+			 ;;(define-key markdown-mode-map "\C-ce" 'sgml-name-char); 文字実体参照HTMLモードを事前にロードしないとダメでうまくいっていない
 			 (define-key markdown-mode-map "\C-ci" 'md-insert-header)))
 
 
