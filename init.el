1488ca25c6f28a6f3d32005b1119dc358981a3ec
Takuya Tsuchida <takuya.tsuchida@gmail.com>
2016-07-18 10:58:09 +0900

    anthy は Xがあるときだけ

--- init.el
+++ init.el
@@ -140,24 +140,26 @@
 	  (require 'server nil t)
 	  (unless (server-running-p)
 		(server-start))
-	  ;; anthy をロードする
-	  (load "anthy")
-	  (setq default-input-method "japanese-anthy")
-	  (setq anthy-accept-timeout nil)
-
-	  (global-unset-key "\C-o")
-      (global-set-key "\C-o" 'toggle-input-method)
-      ;;
-      ;;フレームの設定
-      ;;
-      (setq default-frame-alist
-			(append (list
-					 ;;'(width . 104) ;;フレームの幅
-					 ;;'(height . 47) ;;フレームの高さ
-					 ;;'(font . "fontset-14")
-					 '(font . "-unknown-VL ゴシック-normal-normal-normal-*-13-*-*-*-*-0-iso10646-1")
-					 )
-					default-frame-alist))))
+	  (cond
+	   ;; anthy をロードする(Xが有効な場合のみ)
+	   (window-system
+		(progn
+		  (load "anthy")
+		  (setq default-input-method "japanese-anthy")
+		  (setq anthy-accept-timeout nil)
+		  (global-unset-key "\C-o")
+		  (global-set-key "\C-o" 'toggle-input-method)
+		  ;;
+		  ;;フレームの設定
+		  ;;
+		  (setq default-frame-alist
+				(append (list
+						 ;;'(width . 104) ;;フレームの幅
+						 ;;'(height . 47) ;;フレームの高さ
+						 ;;'(font . "fontset-14")
+						 '(font . "-unknown-VL ゴシック-normal-normal-normal-*-13-*-*-*-*-0-iso10646-1")
+						 )
+						default-frame-alist)))))))
 
 (prefer-coding-system 'utf-8-unix)
 (add-hook 'window-setup-hook
