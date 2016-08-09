d2841740410e53a33714dac7c1a2ef61a7677ec2
Takuya Tsuchida <takuya.tsuchida@gmail.com>
2016-08-09 11:00:32 +0900

    ファイル名が見えないんじゃー

--- conf/my-helm-conf.el
+++ conf/my-helm-conf.el
@@ -7,9 +7,12 @@
 (global-set-key (kbd "C-x C-b") 'helm-mini)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
+;;(global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-q") 'helm-buffers-list)
+(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
 
+(setq helm-buffer-max-length nil)
 (define-key helm-map (kbd "C-h") 'delete-backward-char)
 
 (custom-set-variables
