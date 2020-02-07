d813e72ea7c6cffacc25c34c3263d2e8e503bdb5
Takuya Tsuchida <takuya.tsuchida@gmail.com>
2020-02-07 12:46:47 +0900

    良く分からんがコミットしておこう

--- init.el
+++ init.el
@@ -59,3 +59,17 @@
 (load "my-scala-mode")
 (load "my-docker-mode")
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yasnippet yaml-mode sql-indent package-utils magit helm-ag epl column-marker auto-install auto-complete anything))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
